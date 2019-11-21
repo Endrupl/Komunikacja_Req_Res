b03544f4dea7846b356700cef634130ec130bafb
Andrzej Świderski <31927815+Endrupl@users.noreply.github.com>
2019-11-21 09:58:24 +0100

    Initial commit

---
+++ ProgramRequestResponseServer/ProgramRequestResponseServer/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ProgramRequestResponseServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.windowLogs = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // windowLogs
+            // 
+            this.windowLogs.FormattingEnabled = true;
+            this.windowLogs.Location = new System.Drawing.Point(12, 12);
+            this.windowLogs.Name = "windowLogs";
+            this.windowLogs.Size = new System.Drawing.Size(522, 238);
+            this.windowLogs.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 261);
+            this.Controls.Add(this.windowLogs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox windowLogs;
+    }
+}
+
---
+++ ProgramRequestResponseServer/ProgramRequestResponseServer/Form1.cs
@@ -0,0 +1,57 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramRequestResponseServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Task task = Task.Run(() => server());
+        }
+        void server()
+        {
+            using (var responseSocket = new ResponseSocket("@tcp://*:5555"))
+            {
+                while (true)
+                {
+                    string response = responseSocket.ReceiveFrameString();
+                    switch (response)
+                    {
+                        case "date":
+                            responseSocket.SendFrame(DateTime.Now.ToString("dddd, dd MMMM yyyy"));
+                            break;
+                        case "time":
+                            responseSocket.SendFrame(DateTime.Now.ToString("HH:mm:ss"));
+                            break;
+                    }
+                    MethodInvoker methodInvoker = () => sendLogs(response);
+                    Invoke(methodInvoker);
+                }
+            }
+        }
+
+        void sendLogs(string message)
+        {
+            switch (message)
+            {
+                case "date":
+                    windowLogs.Items.Add(DateTime.Now.ToString() + ". Pomyślnie otrzymaną prośbę o datę");
+                    break;
+                case "time":
+                    windowLogs.Items.Add(DateTime.Now.ToString() + ". Pomyślnie otrzymaną prośbę o czas");
+                    break;
+            }
+        }
+    }
+}
